375b8b84f57d16aeb43413157ad752b97194ebff
Shobhit Juglan <shojug123@gmail.com>
2022-11-28 14:51:38 +0530

    updated extra challenge solution

--- Day2(extra_challenge).py
+++ Day2(extra_challenge).py
@@ -1,14 +1,28 @@
-def check_duplicate(l):
+# def check_duplicate(l):
+#         count = 0
+#         for i in range(len(l)):
+#                 for j in range(i,len(l)-1):
+#                         if l[i-1] == l[j]:
+#                                 return l[i-1]
+#                         else :
+#                                 pass
+#         return "No duplicates"                        
+                
+def check_duplicate(l:list):
         count = 0
         for i in range(len(l)):
-                # for j in range(len(l[i-1])):e
-                for j in range(i,len(l)-1):
-                        if l[i-1] == l[j]:
-                                return l[i-1]
-                        else :
-                                pass
-        return "No duplicates"                        
-                
+                for j in range(i, len(l)-1):
+                        if len(l[i-1]) == len(l[j]):
+                                m = len(l[j])
+                                temp = 0
+                                for k in range(m):
+                                        if ord(l[i-1][k-1]) == ord(l[j][k-1]):
+                                                temp = temp + 1
+                                if m == temp:
+                                        return l[i-1]
+        return "No Duplicates"
+                                         
+
 
 a = int(input("Enter the number of elements in the list\n"))
 listofstrings = []
